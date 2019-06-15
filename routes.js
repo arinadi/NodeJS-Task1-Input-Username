86fe4e6cc3f4f0d4991d1e311084cabd2ffd55f3
arinadi <arinadi.nur@gmail.com>
2019-06-15 14:37:13 +0700

    finish task 1

--- routes.js
+++ routes.js
@@ -5,14 +5,23 @@ const requestHandler = (req, res) => {
     const method = req.method;
     if (url === '/') {
         res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
+        res.write('<head><title>Create Username</title><head>');
         res.write(
-            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+            '<body><h1>Hello, Welcome to this page.<br> Enter your username :</h1><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
         );
         res.write('</html>');
         return res.end();
     }
-    if (url === '/message' && method === 'POST') {
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<head><title>Users List</title><head>');
+        res.write(
+            '<body><h1>Users List :</h1><ul><li>Clara</li><li>Cornor</li></ul></body>'
+        );
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/create-user' && method === 'POST') {
         const body = [];
         req.on('data', chunk => {
             console.log(chunk);
@@ -20,12 +29,11 @@ const requestHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            const username = parsedBody.split('=')[1];
+            console.log(`Username Is ${username}`);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
     }
     res.setHeader('Content-Type', 'text/html');
